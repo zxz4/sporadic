1f586dea26c3e67cb1e8d8922b2e4337c24f6df6
zxz4 <zxz4@msn.com>
2025-03-13 11:49:32 +0800

    update migration to spport lockout feature.

---
+++ framework/Identity/src/Sporadic.Abp.Identity.HttpApi.Host/Migrations/20250313034850_AddLockoutToUser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLockoutToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AccessFailedCount",
+                table: "SYS_Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "LockoutEnabled",
+                table: "SYS_Users",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LockoutEnd",
+                table: "SYS_Users",
+                type: "datetime(6)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccessFailedCount",
+                table: "SYS_Users");
+
+            migrationBuilder.DropColumn(
+                name: "LockoutEnabled",
+                table: "SYS_Users");
+
+            migrationBuilder.DropColumn(
+                name: "LockoutEnd",
+                table: "SYS_Users");
+        }
+    }
+}
